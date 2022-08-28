180e214f06d5a2643bad6586cafc77c14a2033d2
MP <mantas.peciukevicius@gmail.com>
2022-08-28 21:49:29 +0300

    JavaScript Fundamentals – Part 1

---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log("test");
+
+// let = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1923;
+// // const job;
+// var job = "rogrammer";
+// job = "tester";
+
+// lasTane = "peckus";
+// console.log(lasTane);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, Math.round((ageJonas / 6) * 10) / 10, 2 ** 3);
+// auksciau padauginta is 10, tada suapvalinta, tada padalinta is 10 tam, kad butu vienas skaicius po kablelio
+const firstName = "Mantas";
+const lastName = "Peckus";
+console.log(firstName + " " + lastName);
+console.log(typeof firstName, typeof lastName);
+console.log("test");
