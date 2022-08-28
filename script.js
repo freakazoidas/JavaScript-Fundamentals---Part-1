a7b34b90f0326de66c798e3de94edb0be9e5167f
MP <mantas.peciukevicius@gmail.com>
2022-08-28 22:10:18 +0300

    15. Operator precendence

--- script.js
+++ script.js
@@ -11,14 +11,26 @@
 
 // lasTane = "peckus";
 // console.log(lasTane);
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2, Math.round((ageJonas / 6) * 10) / 10, 2 ** 3);
-// auksciau padauginta is 10, tada suapvalinta, tada padalinta is 10 tam, kad butu vienas skaicius po kablelio
-const firstName = "Mantas";
-const lastName = "Peckus";
-console.log(firstName + " " + lastName);
-console.log(typeof firstName, typeof lastName);
-console.log("test");
+// Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, Math.round((ageJonas / 6) * 10) / 10, 2 ** 3);
+// // auksciau padauginta is 10, tada suapvalinta, tada padalinta is 10 tam, kad butu vienas skaicius po kablelio
+// const firstName = "Mantas";
+// const lastName = "Peckus";
+// console.log(firstName + " " + lastName);
+// console.log(typeof firstName, typeof lastName);
+// // Assigment operators
+// let x = 10 + 5; // x = 15
+// x += 10; // x = x + 15 = 25
+// x *= 4; // x= x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+// // comparison operators
+// console.log(ageJonas > ageSarah); // >,<,<+, >=
+// console.log(ageSarah >= 18);
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018);
