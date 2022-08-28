830ccf217d0152a2f2f8845acc128822681a339b
MP <mantas.peciukevicius@gmail.com>
2022-08-29 01:25:36 +0300

    16 first challenge

--- script.js
+++ script.js
@@ -34,3 +34,7 @@
 // console.log(ageSarah >= 18);
 // const isFullAge = ageSarah >= 18;
 // console.log(now - 1991 > now - 2018);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
